4f690f5450256bf8db1ddadbd2d465d68d1fc1c8
Amit <abcdamit44@gmail.comm>
2022-06-29 11:33:03 +0530

    Fix Bugs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,7 +4,16 @@ import React from 'react';
 const Blog = () => {
     return (
         <>
-            Blog
+            {/* <!-- Blog Section --> */}
+            <div className="container" id="projects">
+                <div className="projects">
+                    <h2>Blog</h2>
+                    <div className="d-flex justify-content-between">
+
+                    </div>
+                </div>
+            </div>
+            {/* <!-- Blog Section END --> */}
 
         </>
     )
